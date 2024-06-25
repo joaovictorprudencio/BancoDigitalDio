a5a5c193f46a11f84e7bad85984d6eba7cf157c9
joao victor <joaovprudencio13@gmail.com>
2024-06-25 15:24:32 -0300

    feito

--- src/App.java
+++ src/App.java
@@ -1,5 +1,15 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Cliente joaoVictor = new Cliente();
+		joaoVictor.setNome("João victor");
+		
+		Conta cc = new ContaCorrente(joaoVictor);
+		Conta poupanca = new ContaPoupanca(joaoVictor);
+
+		cc.depositar(5000);
+		cc.transferir(1000, poupanca);
+		
+		cc.imprimirExtrato();
+		poupanca.imprimirExtrato();
     }
 }
